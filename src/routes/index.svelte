2d485ca88e618a3c1d285e6acbf2d343c6b5299f
squizlee <ethanscavia777@gmail.com>
2021-08-25 15:55:37 +1000

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Footer from "../components/Footer.svelte";
+</script>
+
+
+<header>
+	<h1>Black Canvas</h1>
+	<p>Create Art With Code</p>
+</header>
+
+<p>Currently under development</p>
+<hr>
+<section id="banner">
+	<p id="code"></p>
+	<p id="description">Platform for your code and creations</p>
+</section>
+
+<section id="details">
+	<p>Black canvas is a platform for art creation using a programming language - a dialect of Lisp.</p>
+	<p><strong>Features include:</strong></p>
+	<ul>
+		<li>Platform for sharing your creations.</li>
+		<li>Sign up is optional.</li>
+		<li>Try an alternative method for your creative mind.</li>
+	</ul>
+</section>
+
+<section id="call-to-action">
+	<p>Find out more</p>
+	<button>Let's begin</button>
+</section>
+
+<Footer />
