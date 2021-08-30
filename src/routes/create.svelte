0044c3ae8b48ec6ab2bb8f25fc0b332235fc94a6
squizlee <ethanscavia777@gmail.com>
2021-08-30 17:31:33 +1000

    Create page skeleton and Assets
    - Images for landing page
    - Credits page for creditation of icons and assets

---
+++ src/routes/create.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { onMount } from "svelte";
+	
+	onMount(() => {
+		const canvas = document.getElementById("preview");
+		// @ts-ignore
+		const ctx = canvas.getContext("2d");
+		canvas.style.width = "100%";
+		ctx.fillStyle = "green";
+		ctx.fillRect(10,10,150,100);
+	});
+
+</script>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 3fr 0.5fr;
+		min-height: 100vh;
+		max-height: 100vh;
+	}
+	
+	#text-editor {
+		border: 1px solid red;
+		grid-row-start: 1;
+		padding: 2rem;
+	}
+	
+	#debug {
+		border: 1px solid blue;
+		grid-row-start: 2;
+	}
+	
+	#preview {
+		border: 1px solid white;
+		grid-row-start: 1;
+		grid-row-end: 3;
+		width: 100%;
+		margin: auto;
+		max-height: 80vh;
+	}
+	
+</style>
+
+
+<main>
+	<section id="text-editor" contenteditable>TEXT EDITOR</section>
+	<section id="debug">
+		<em class="hint-txt">This is the debug screen all (debug) commands will be shown here</em>
+		<p id="debug-output"></p>
+	</section>
+	<canvas id="preview"></canvas>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,51 @@
-<script>
-	import Footer from "../components/Footer.svelte";
-</script>
+<style>
+	section {
+		width: 60%;
+		margin: 0 auto;
+	}
+	
+	header {
+		position: relative;
+		padding-top: 8rem;
+		text-align: center;
+	}
+	
+	header img {
+		position: absolute;
+		z-index: -1;
+		transform: scale(0.55);
+		filter: drop-shadow(78px -73px 4px #201924);
+		top: 0;
+		left: 25%;
+	}
+	
+	header > * {
+		margin: 8px;
+	}
+	
+	header h1 {
+		font-size: 4rem;
+	}
+	
+	header .subtitle {
+		font-size: 2.5rem;
+		color: var(--light-purple);
+		padding-left: 5rem;
+	}
 
+</style>
 
 <header>
+	<img src="/images/open_air_painter_winter.png" alt="A man painting">
 	<h1>Black Canvas</h1>
-	<p>Create Art With Code</p>
+	<p class="subtitle">CREATE ART WITH CODE</p>
 </header>
 
-<p>Currently under development</p>
-<hr>
+<section id="disclaimer">
+	<p>Currently under development</p>
+	<hr>
+</section>
+
 <section id="banner">
 	<p id="code"></p>
 	<p id="description">Platform for your code and creations</p>
@@ -27,7 +63,6 @@
 
 <section id="call-to-action">
 	<p>Find out more</p>
-	<button>Let's begin</button>
+	<a href="/create"><button>Let's begin</button></a>
 </section>
 
-<Footer />
