ab7549b8abbcbf670014439c2335e39b95c8dae5
squizlee <ethanscavia777@gmail.com>
2021-11-09 14:29:07 +1100

    Get user info on create page

--- src/components/CreateUI.svelte
+++ src/components/CreateUI.svelte
@@ -1,20 +1,37 @@
 <script>
-// UI and Logic for handling UI events
-import axios from "axios";
-import {program_source} from "../state/program_source.js";
+	// UI and Logic for handling UI events
+	import axios from "axios";
+	import { program_source } from "../state/program_source.js";
+	import { onMount } from "svelte";
 
-function submit() {
-	console.log("submit", $program_source);
-}
+	let user = "anon";
 
-function save() {
-	console.log("save");
-}
+	onMount(async () => {
+		try {
+			let { data } = await axios.get(
+				"http://localhost:8000/api/profile",
+				{
+					withCredentials: true,
+				}
+			);
+			user = data.username;
+		} catch {
+			console.log("Error pinging server");
+		}
+	});
 
+	function submit() {
+		console.log("submit", $program_source);
+	}
+
+	function save() {
+		console.log("save");
+	}
 </script>
 
 <section id="create-ui">
 	<button on:click={submit}>submit</button>
+	<p>Logged in as <span>{user}</span></p>
 </section>
 
 <style>
@@ -22,5 +39,15 @@ function save() {
 		display: flex;
 		position: absolute;
 		top: 0;
+		height: 2rem;
+		align-items: center;
+	}
+
+	#create-ui > * {
+		margin-right: 1.25rem;
+	}
+
+	span {
+		color: yellow;
 	}
 </style>
--- src/routes/create.svelte
+++ src/routes/create.svelte
@@ -1,37 +1,35 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
 	import { onMount } from "svelte";
 	import CreateUI from "../components/CreateUI.svelte";
 	import TextEditor from "../components/TextEditor.svelte";
 	import env from "../blisp/env.js";
-	
+
 	let canvas;
 	let ctx;
-	
 
 	onMount(() => {
 		canvas = document.getElementById("canvas");
 		let container = document.getElementById("canvas-container");
-		
+
 		ctx = canvas.getContext("2d");
 		ctx.imageSmoothingEnabled = true;
 		canvas.width = container.clientHeight;
 		canvas.height = container.clientHeight;
-		
+
 		window.onresize = () => {
 			canvas.width = container.clientHeight;
 			canvas.height = container.clientHeight;
 			env._width = canvas.width;
 			env._height = canvas.height;
-			
-		}
-		
+		};
+
 		canvas.onclick = (e) => {
 			let x = e.layerX;
 			let y = e.layerY;
 			console.log(x, y);
-		}
+		};
 
 		// populate ctx
 		env._ctx = ctx;
@@ -39,9 +37,24 @@
 		env._height = canvas.height;
 		console.log(env);
 	});
-	
-
 </script>
+
+<main>
+	<CreateUI />
+	<section id="text-editor-container">
+		<TextEditor />
+	</section>
+	<section id="debug">
+		<em class="hint-txt"
+			>This is the output screen, all evaluations and (debug) commands
+			will go here</em
+		>
+		<p id="debug-output" />
+	</section>
+	<section id="canvas-container">
+		<canvas id="canvas" />
+	</section>
+</main>
 
 <style>
 	main {
@@ -51,51 +64,35 @@
 		min-height: 100vh;
 		max-height: 100vh;
 	}
-	
+
 	#text-editor-container {
 		grid-row-start: 1;
 		padding: 2rem;
 		padding-left: 20px;
 		min-width: 100%;
 	}
-	
+
 	#debug {
 		grid-row-start: 2;
 	}
-	
+
 	canvas {
 		display: block;
 		margin: 0 auto;
 		outline: 1px dashed white;
 	}
-	
+
 	#canvas-container {
 		grid-row-start: 1;
 		grid-row-end: 3;
 		height: 100vh;
 		width: 100%;
 	}
-	
+
 	@media screen and (max-width: 1000px) {
 		main {
 			display: flex;
 			flex-direction: column;
 		}
 	}
-	
 </style>
-
-
-<main>
-	<CreateUI />
-	<section id="text-editor-container">
-		<TextEditor />
-	</section>
-	<section id="debug">
-		<em class="hint-txt">This is the output screen, all evaluations and (debug) commands will go here</em>
-		<p id="debug-output"></p>
-	</section>
-	<section id="canvas-container">
-		<canvas id="canvas"></canvas>
-	</section>
-</main>
