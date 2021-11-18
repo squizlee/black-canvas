49bbdc379979ee0aedc62145f30866fbea4ce246
squizlee <ethanscavia777@gmail.com>
2021-11-18 16:30:35 +1100

    Readme installation instructions

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Create art with code (a lisp like language called blisp). The language is interp
 **CURRENTLY IN DEVELOPMENT**
 
 see `documentation/blisp, documentation/example_programs` for info regarding the language.
+
+# Installation & Running
+
+1. Node JS Version 16 should be installed
+2. `npm install`
+3. `npm run dev`
+
+Note if you want to use account functionality you must be on a unix system. Use `npm run db:init` to initalize the database. In another terminal `npm run dserver` to run the development server. The program can run without the development server.
+
+# Using the language
+
+Type in the text editor blisp source code and press <Ctrl> + <Enter> to execute the program.
--- src/temp_script.js
+++ src/temp_script.js
@@ -1,4 +1,4 @@
-let source = `
+let example_one = `
 ;; simple canvas api operations
 ;; variables and math
 (set-fillStyle "green")
@@ -9,5 +9,41 @@ let source = `
 (fillRect 10 300 250 250)
 
 `;
+
+let better = `
+(set-fillStyle "white")
+(set-strokeStyle "grey")
+(fillRect 25 25 100 100)
+(clearRect 45 45 60 60)
+(strokeRect 50 50 50 50)
+`;
+
+let house = `
+(set-context "grid" 25)
+(set-fillStyle "green")
+(set-strokeStyle "white")
+
+;; base
+(fillRect 200 200 100 100)
+;; roof
+(set-fillStyle "white")
+(beginPath)
+(moveTo 200 200)
+(lineTo 250 150)
+(lineTo 300 200)
+(fill)
+(closePath)
+
+;; door
+
+(set-fillStyle "red")
+(fillRect 225 260 25 40)
+
+;; windows 
+(set-fillStyle "blue")
+(fillRect 270 250 25 25)
+`;
+
+let source = house;
 
 export default source;
